c95100149b09934e81ca75700ffe3189ee5b0b12
idsc3101 <idsc3101@gmail.com>
2013-10-16 16:07:15 -0500

    Workday 7

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -10,10 +10,20 @@ Public Class Form1
         ' Compute the result
         Dim result As String = ""
 
+        Dim i As Integer
+        For i = st.Length - 1 To 0 Step -1
+            'length starts at zero so that last character is 4
+            Dim c As Char
+            c = st.Chars(i)
+
+            result = result & c
+            'take character from front and add it to the back and then shift everything over
+            'result= "he" & "l" so just switch the c and result in order to switch the order build it from the right
+        Next
 
 
         ' Report the output to the user
-        MessageBox.Show("Not implemented yet", "Result")
+        MessageBox.Show(result, "Result")
 
     End Sub
 End Class
